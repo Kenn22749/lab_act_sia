a6ec65ecd916217e8c09993645bc5240cea50c7f
Kenn22749 <saldivarkenn2@gmail.com>
2026-03-10 11:03:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,29 @@
-function openCar(car){
+function showInfo(car){
 
-let title=document.getElementById("carTitle")
-let desc=document.getElementById("carDescription")
-let img=document.getElementById("modalImg")
+let title = document.getElementById("carTitle")
+let desc = document.getElementById("carDescription")
+let box = document.getElementById("infoBox")
 
-let sound = new Audio(car + ".mp3")
-
-if(car==="r34"){
-
-title.innerText="Nissan Skyline R34 GTR"
-
-desc.innerText="The R34 GTR is one of the most legendary JDM cars ever built. It features the RB26 twin turbo engine and the advanced ATTESA AWD system."
-
-img.src="r34.jpg"
-
+if(car === "r34"){
+title.innerText = "Nissan Skyline R34 GTR"
+desc.innerText = "The R34 GTR is one of the most iconic JDM cars ever made. Powered by the legendary RB26DETT twin-turbo engine and known from Fast & Furious, it represents the peak of Japanese performance engineering."
 }
 
-if(car==="rx7"){
-
-title.innerText="Mazda RX7"
-
-desc.innerText="The RX7 uses a rotary engine which makes it lightweight and extremely fun to drive. It became a JDM icon thanks to drifting culture."
-
-img.src="rx7.jpg"
-
+if(car === "rx7"){
+title.innerText = "Mazda RX-7"
+desc.innerText = "The RX-7 is famous for its lightweight design and rotary engine. Its unique sound and incredible handling made it one of the most loved sports cars in the JDM community."
 }
 
-if(car==="nsx"){
-
-title.innerText="Honda NSX"
-
-desc.innerText="The Honda NSX was developed with input from Formula 1 legend Ayrton Senna. It was designed to rival Ferrari while maintaining reliability."
-
-img.src="nsx.jpg"
-
+if(car === "nsx"){
+title.innerText = "Honda NSX"
+desc.innerText = "The Honda NSX was Japan’s answer to Ferrari. Developed with input from Ayrton Senna, it combines exotic performance with Honda reliability."
 }
 
-if(car==="evox"){
-
-title.innerText="Mitsubishi Lancer Evolution X"
-
-desc.innerText="The Evo X is a turbocharged AWD rally machine known for its aggressive styling and insane cornering ability."
-
-img.src="evox.jpg"
-
+if(car === "evox"){
+title.innerText = "Mitsubishi Lancer Evolution X"
+desc.innerText = "The Evo X is a rally-inspired monster with all-wheel drive and turbocharged power. Known for its aggressive styling and incredible cornering ability."
 }
 
-document.getElementById("modal").style.display="flex"
-
-sound.play()
-
-}
-
-function closeCar(){
-
-document.getElementById("modal").style.display="none"
+box.style.display = "block"
 
 }
